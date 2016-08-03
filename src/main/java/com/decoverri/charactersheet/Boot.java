ce3bc4280d2c03a0cda545221672bb2767f72bee
Deco Verri <decoverri@gmail.com>
2016-08-03 16:38:39 -0300

    Starting project with SetupMyProject

---
+++ src/main/java/com/decoverri/charactersheet/Boot.java
@@ -0,0 +1,14 @@
+package com.decoverri.charactersheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+}
